2a8c74345c3a1cd0576a7b5b5001111a2d12a1c0
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2024-09-29 10:29:00 -0400

    deploy

--- sst.config.ts
+++ sst.config.ts
@@ -14,6 +14,18 @@ export default $config({
     };
   },
   async run() {
-    new sst.aws.Nextjs("Site");
+    new sst.aws.Nextjs("Site", {
+      domain: (() => {
+        if ($app.stage === "production") {
+          return {
+            name: "kylewigs.com",
+            redirects: ["www.kylewigs.com"],
+            dns: sst.cloudflare.dns({
+              zone: "1b803a1637ea5109fe7e3f2141291a96"
+            })
+          }
+        }
+      })()
+    });
   },
 });
